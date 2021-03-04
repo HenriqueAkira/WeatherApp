644b54b03c7e2867bf7801a268a52021d360bf2b
HenriqueAkira <henrique.yasuda@hotmail.com>
2021-03-04 09:28:48 -0300

    Repo created

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.jsfafafa to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
